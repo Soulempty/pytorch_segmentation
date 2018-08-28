cbf2d340257b716eed36c309595819678a54afa7
Chao Jiao <32031469+Soulempty@users.noreply.github.com>
2018-08-28 04:51:49 -0400

    Update test.py

--- ICNet_pytorch/test.py
+++ ICNet_pytorch/test.py
@@ -13,10 +13,6 @@ from resnet import ICNet,interp,Bottleneck,PSPDec
 IS_MULTISCALE = True
 N_CLASS = 2
 is_local=True
-COLOR_MAP = [(128, 64, 128), (244, 35, 232), (70, 70, 70), (102, 102, 156), (190, 153, 153), (153, 153, 153),
-             (250, 170, 30), (220, 220, 0), (107, 142, 35), (152, 251, 152), (70, 130, 180), (220, 20, 60),
-             (255,  0,  0), (0, 0, 142), (0, 0, 70), (0, 60, 100), (0, 80, 100), (0, 0, 230), (119, 11, 32)]
-
 inf_scales = [0.5, 0.75, 1.0, 1.25, 1.5, 1.8]
 data_transforms = transforms.Compose([transforms.ToTensor(),
                                       transforms.Normalize([0.5, 0.5, 0.5],
